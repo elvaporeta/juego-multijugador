a615b30c0f0c710f780bbed1828ce6a24a5849e8
elvaporeta <gaalcast.2009@gmail.com>
2025-04-16 10:51:37 -0700

    Arreglando config para Render

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ let counter = 0;
 io.on('connection', socket => {
   console.log('Jugador conectado:', socket.id);
 
+  // Enviar la lista actualizada a todos los jugadores
+  io.emit("update-player-list", players);
+
+  socket.on("disconnect", () => {
+    console.log("Jugador desconectado:", socket.id);
+    players = players.filter(id => id !== socket.id);
+    io.emit("update-player-list", players);
+
   // Enviar el contador actual al jugador nuevo
   socket.emit('counter', counter);
 
